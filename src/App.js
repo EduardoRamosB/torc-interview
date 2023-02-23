2b2661c89bda637a1b53ebbce1fc844b2f41a886
EduardoRamosB <eduardo.ramos.b1@gmail.com>
2023-02-23 13:27:39 -0500

    Cleaning code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css'
 import {useState} from "react"
 import ListUser from "./components/ListUser"
 
@@ -8,7 +7,6 @@ function App() {
   const [l3, setL3] = useState(['g1', 'g2', 'g3'])
 
   const changeItem = (row, direction, clickList, sideList) => {
-    console.log('row:', row, 'direction:', direction, 'clickList:', clickList, 'sideList:', sideList)
     let newSideList = []
     const newClickList = [...clickList]
 
@@ -17,41 +15,15 @@ function App() {
       case 2: newSideList = [...l2];break;
       case 3: newSideList = [...l3];break;
     }
-    console.log('newSideList:', newSideList)
 
-    if(direction === 'right'){
-      const currentList = sideList - 1 < 1 ? 3 : sideList - 1
-      console.log(currentList)
-      newSideList.splice(row, 0, clickList[row])
-      newClickList.splice(row, 1)
-      console.log('newSideList:', newSideList, 'newClickList:', newClickList)
-      updateLists(currentList, newClickList, newSideList, direction)
-    } else {
-      const currentList = sideList + 1 > 3 ? 1 : sideList + 1
-      console.log(currentList)
-      newSideList.splice(row, 0, clickList[row])
-      newClickList.splice(row, 1)
-      console.log('newSideList:', newSideList, 'newClickList:', newClickList)
-      updateLists(currentList, newClickList, newSideList, direction)
-    }
+    newSideList.splice(row, 0, clickList[row])
+    newClickList.splice(row, 1)
 
+    const currentList = direction === 'right' ?
+      sideList - 1 < 1 ? 3 : sideList - 1 :
+      sideList + 1 > 3 ? 1 : sideList + 1
 
-
-    /*const newL1 = [...l1]
-    const newL2 = [...l2]
-
-    if(direction === 'right'){
-      newL2.splice(row, 0, l1[row])
-      newL1.splice(row, 1)
-    } else {
-      newL1.splice(row, 0, l2[row])
-      newL2.splice(row, 1)
-    }
-
-
-    setL2(newL2)
-    setL1(newL1)*/
-
+    updateLists(currentList, newClickList, newSideList, direction)
   }
 
   const updateLists = (currentList, newClickList, newSideList, direction) => {
@@ -74,15 +46,6 @@ function App() {
   return (
     <div className='container'>
       <h1>To Do List</h1>
-      {/*
-        l1.map((e, i) =>
-          <p key={i}>
-            <Button direction='left' changeItem={changeItem} row={i}/>{e}<Button direction='right' changeItem={changeItem} row={i}/>
-            <Button direction='left' changeItem={changeItem} row={i}/>{l2[i]}<Button direction='right' changeItem={changeItem} row={i}/>
-            {l3[i]}
-          </p>
-        )
-      */}
       <div className="row">
         <div className="col-md-3">
           <ListUser name={'John'} list={l1} changeItem={changeItem} listIdx={1}/>
@@ -91,7 +54,7 @@ function App() {
           <ListUser name={'Paul'} list={l2} changeItem={changeItem} listIdx={2}/>
         </div>
         <div className="col-md-3">
-          {<ListUser name={'George'} list={l3} changeItem={changeItem} listIdx={3}/>}
+          <ListUser name={'George'} list={l3} changeItem={changeItem} listIdx={3}/>
         </div>
       </div>
     </div>
